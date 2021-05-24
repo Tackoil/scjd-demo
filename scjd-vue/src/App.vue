b68360e6c1388daae75e4553a4b0053dd9a978e3
Tangnameless <tang0079@foxmail.com>
2021-05-24 14:44:20 +0800

    在app.vue中加了一个map
    依赖环境增加echarts v4.2.1
    components/Charts中存放图表子组件
    写了一个新疆各州地专利授权情况的可视化地图。

--- scjd-vue/src/App.vue
+++ scjd-vue/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <el-container style="height: 100vh">
-    <el-header height="70px" >新疆市场监督管理局大数据平台</el-header>
+    <el-header height="70px">新疆市场监督管理局大数据平台</el-header>
     <el-main>
       <transition name="el-fade-in-linear"> </transition>
+      <div class="chart-container">
+        <ChartMap height="100%" width="100%" />
+      </div>
     </el-main>
-    <el-footer height="70px"> 北京邮电大学电子工程学院通信与网络研究中心（2021） </el-footer>
+    <el-footer height="70px">
+      北京邮电大学电子工程学院通信与网络研究中心（2021）
+    </el-footer>
   </el-container>
 </template>
 
 <script>
 // import Splitter from 'primevue/splitter';
 // import SplitterPanel from 'primevue/splitterpanel';
+import ChartMap from "@/components/Charts/Map";
 
 export default {
   name: "App",
   // components: {Splitter, SplitterPanel},
+  components: { ChartMap },
   methods: {},
 };
 </script>
@@ -55,5 +62,10 @@ body {
   background-color: rgb(245, 248, 251);
   padding-bottom: 0;
   overflow: hidden;
+}
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 84px);
 }
 </style>
