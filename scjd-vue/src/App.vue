6d3fab90a99e7774093149885f9fb27066345d71
Tackoil <santoin351@outlook.com>
2021-05-26 09:42:28 +0800

    上传了一些可以拖动的框框

--- scjd-vue/src/App.vue
+++ scjd-vue/src/App.vue
@@ -2,28 +2,72 @@
   <el-container style="height: 100vh">
     <el-header height="70px">新疆市场监督管理局大数据平台</el-header>
     <el-main>
-      <transition name="el-fade-in-linear"> </transition>
-      <div class="chart-container">
-        <PatentMap height="100%" width="100%" />
-      </div>
+      <el-scrollbar>
+        <div style="width: 100%">
+          <div>
+            <div class="layoutJSON">
+              Displayed as <code>[x, y, w, h]</code>:
+              <div class="columns">
+                <div class="layoutItem" v-for="item in layout" :key="item.i">
+                  <b>{{ item.i }}</b
+                  >: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="content">
+            <grid-layout
+              v-model:layout="layout"
+              :col-num="12"
+              :row-height="30"
+              is-draggable is-resizable vertical-compact
+              :use-css-transforms="true"
+            >
+              <grid-item
+                v-for="item in layout"
+                :key="item.i"
+                :x="item.x"
+                :y="item.y"
+                :w="item.w"
+                :h="item.h"
+                :i="item.i"
+              >
+                <el-card style="height: 100%" :body-style="{height: '100%'}">
+                  <patent-line-race id="a" v-if="item.i === '0'" width="100%" height="100%"/>
+                  <patent-pie id="b" v-if="item.i === '1'" width="100%" height="100%" />
+                </el-card>
+              </grid-item>
+            </grid-layout>
+          </div>
+        </div>
+      </el-scrollbar>
     </el-main>
-    <el-footer height="70px">
-      北京邮电大学电子工程学院通信与网络研究中心（2021）
-    </el-footer>
   </el-container>
 </template>
 
 <script>
-// import Splitter from 'primevue/splitter';
-// import SplitterPanel from 'primevue/splitterpanel';
-import PatentMap from "@/components/Charts/PatentLine";
+import PatentPie from "@/components/Charts/PatentPie";
+import PatentLineRace from "@/components/Charts/PatentLineRace";
+const testLayout = [
+  { x: 0, y: 0, w: 2, h: 2, i: "0" },
+  { x: 2, y: 0, w: 2, h: 4, i: "1" },
+  { x: 4, y: 0, w: 2, h: 5, i: "2" },
+  { x: 6, y: 0, w: 2, h: 3, i: "3" },
+];
 
 export default {
   name: "App",
-  // components: {Splitter, SplitterPanel},
-  components: { PatentMap },
-  methods: {},
-};
+  components: {PatentPie, PatentLineRace},
+  data() {
+    return {
+      layout: JSON.parse(JSON.stringify(testLayout)),
+      draggable: true,
+      resizable: true,
+      compact: true,
+      selectedTab: null,
+    };
+  },
+}
 </script>
 
 <style>
@@ -67,5 +111,53 @@ body {
   position: relative;
   width: 100%;
   height: calc(100vh - 84px);
+}
+
+.columns {
+    -moz-columns: 120px;
+    -webkit-columns: 120px;
+    columns: 120px;
+}
+.vue-resizable-handle {
+    z-index: 5000;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    bottom: 0;
+    right: 0;
+    background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/Pg08IS0tIEdlbmVyYXRvcjogQWRvYmUgRmlyZXdvcmtzIENTNiwgRXhwb3J0IFNWRyBFeHRlbnNpb24gYnkgQWFyb24gQmVhbGwgKGh0dHA6Ly9maXJld29ya3MuYWJlYWxsLmNvbSkgLiBWZXJzaW9uOiAwLjYuMSAgLS0+DTwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DTxzdmcgaWQ9IlVudGl0bGVkLVBhZ2UlMjAxIiB2aWV3Qm94PSIwIDAgNiA2IiBzdHlsZT0iYmFja2dyb3VuZC1jb2xvcjojZmZmZmZmMDAiIHZlcnNpb249IjEuMSINCXhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHhtbDpzcGFjZT0icHJlc2VydmUiDQl4PSIwcHgiIHk9IjBweCIgd2lkdGg9IjZweCIgaGVpZ2h0PSI2cHgiDT4NCTxnIG9wYWNpdHk9IjAuMzAyIj4NCQk8cGF0aCBkPSJNIDYgNiBMIDAgNiBMIDAgNC4yIEwgNCA0LjIgTCA0LjIgNC4yIEwgNC4yIDAgTCA2IDAgTCA2IDYgTCA2IDYgWiIgZmlsbD0iIzAwMDAwMCIvPg0JPC9nPg08L3N2Zz4=');
+    background-position: bottom right;
+    padding: 0 3px 3px 0;
+    background-repeat: no-repeat;
+    background-origin: content-box;
+    box-sizing: border-box;
+    cursor: se-resize;
+}
+
+.vue-grid-item.resizing {
+    opacity: 0.9;
+}
+.vue-grid-item .text {
+    font-size: 24px;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: 24px;
+}
+.vue-grid-item .minMax {
+    font-size: 12px;
+}
+.vue-grid-item .add {
+    cursor: pointer;
+}
+.remove {
+    position: absolute;
+    right: 2px;
+    top: 0;
+    cursor: pointer;
 }
 </style>
--- scjd-vue/src/main.js
+++ scjd-vue/src/main.js
@@ -1,19 +1,15 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/saga-blue/theme.css'       //theme
-import 'primevue/resources/primevue.min.css'                 //core css
-import 'primeicons/primeicons.css'                           //icons
-
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 
+import VueGridLayout from 'vue-grid-layout'
+
 const app = createApp(App);
 
-app.use(PrimeVue);
-app.config.globalProperties.$primevue = {ripple: true};
-
 app.use(ElementPlus);
+
+app.use(VueGridLayout)
 
 app.mount('#app');
